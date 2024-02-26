0f0644a21f7d673078edd40bd24060d809123167
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2024-02-26 17:05:42 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Contact Us Page Android
 
-This is simple `Dart` `Flutter` front-end `Contact Us` page for android app.
+Simple `Dart` `Flutter` `Contact Us` page for android app.
 
 ![contact_us.png](images%2Fcontact_us.png)
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/avatar.png'),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              const Text(
+                'Your Name',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Peralta',
+                ),
+              ),
+              Text(
+                'android developer'.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.blueGrey[100],
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'MontaguSlab',
+                  letterSpacing: 1.5,
+                ),
+              ),
+              Divider(
+                color: Colors.blueGrey[300],
+                thickness: 1.0,
+                height: 20.0,
+                indent: 100.0,
+                endIndent: 100.0,
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.phone,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    '+000000000000000',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueGrey[700],
+                      fontFamily: 'Roboto',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.email,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'example@example.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueGrey[700],
+                      fontFamily: 'Roboto',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
